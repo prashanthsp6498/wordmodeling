a6272a571624ccd64a09c56347720de25c2b512d
Prashantha SP <prashanth.sp98@gmail.com>
2020-04-28 18:00:59 +0530

    integrated model object with webapp

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import os
 from flask import Flask, render_template, request, jsonify
+from .model.predictor import Predictor
 
 app = Flask(__name__)
+
+os.chdir('model')
+
+
+def WordSuggestion(word):
+    preditorObject = Predictor()
+    return preditorObject.textGenerator(word)
 
 
 @app.route('/')
 def index():
     num = ['who', 'am', 'i', 'and', 'for', 'do']
     number = [0, 1, 2, 3, 4, 5, 6]
+    num = WordSuggestion('hello')
     return render_template('word_predictor.html', num=num, number=number)
