c787dba05ec9c10ac630aac51384d6e05d4fd28e
kushtej <007kushtej@gmail.com>
2020-06-06 13:57:06 +0530

    automated email addition and starting dev server

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+read -p "Enter EMAIL: " email
+read -p "Enter PASSWORD: " password
+read -p "Confirm EMAIL or PASSWORD? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+gmail='**'$email'**'
+gmail_password='**'$password'**'
+
+export mail=gmail
+export password=gmail_password
+
 export FLASK_APP=run.py
 flask run
